e9303644dc3ee148e24d7adc3c0dbbd4c0d81685
d4rkfly3r <npjoshf@gmail.com>
2016-01-11 00:27:11 -0800

    Just laying things out

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -6,5 +6,4 @@ public class MainClass {
     public static void main(String[] args) {
         System.out.println("Starting....");
     }
-
 }
---
+++ src/test/java/TestMainClass.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+/**
+ * Created by Joshua on 1/11/2016.
+ */
+public class TestMainClass {
+
+    @Test
+    public void TestMain() {
+
+    }
+}
